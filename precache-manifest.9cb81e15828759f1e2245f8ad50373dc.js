124a28ba31ddfb412c02e5e1a9d43ea4ed95f956
sangharsh <49234331+sangharsh1008@users.noreply.github.com>
2020-01-07 15:58:44 +0530

    testCommit

---
+++ precache-manifest.9cb81e15828759f1e2245f8ad50373dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf597f412ba0907189d97d0686ba617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7402405bf07e6d86b374",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "65467f1a0c1d176c5caa",
+    "url": "/static/css/main.efd958ff.chunk.css"
+  },
+  {
+    "revision": "7402405bf07e6d86b374",
+    "url": "/static/js/2.1ef8b5f1.chunk.js"
+  },
+  {
+    "revision": "65467f1a0c1d176c5caa",
+    "url": "/static/js/main.8dfb9f9e.chunk.js"
+  },
+  {
+    "revision": "d12934e3fc397b36f3d2",
+    "url": "/static/js/runtime-main.5d51109e.js"
+  }
+]);
